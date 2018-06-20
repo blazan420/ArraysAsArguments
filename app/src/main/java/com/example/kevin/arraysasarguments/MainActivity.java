c091c22dca8c8e2ddf469d8260e42188419e8924
kevin <kevinjobrien420@gmail.com>
2018-06-19 20:44:25 -0500

    215 App Number 24 - Part1 - ArraysAsArguments // Coding

--- app/src/main/java/com/example/kevin/arraysasarguments/MainActivity.java
+++ app/src/main/java/com/example/kevin/arraysasarguments/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.kevin.arraysasarguments;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,28 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        TextView  txtArrayValues = (TextView) findViewById(R.id.txtArrayValues);
+        String oldTxtArrayValues;
+        TextView txtAverageValue = (TextView) findViewById(R.id.txtAverageValue);
+
+        int[] intArray = {2, 4, 54, 6, 7 ,43, 23, 75, 7, 8, 4, 22, 89};
+
+        for (int index = 0; index < intArray.length; index++) {
+            oldTxtArrayValues = txtArrayValues.getText().toString();
+            txtArrayValues.setText(oldTxtArrayValues + intArray[index] + "      ");
+        }
+
+
+    }
+
+    public int getTheAverageValue(int... numbers) {
+        int totalValue = 0;
+        for (int value : numbers) {
+            totalValue += value;
+
+        }
+        return totalValue / numbers.length;
     }
 }
